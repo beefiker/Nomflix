4c4816ac02165985f29c6e57428772404311e053
J young <beefiker@gmail.com>
2021-05-02 21:40:57 +0900

    add how to sexy networking with Axios

--- src/api.js
+++ src/api.js
@@ -8,4 +8,34 @@ const api = axios.create({
   },
 });
 
-export default api;
+export const moviesApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  search: (term) =>
+    api.get("search/movie", {
+      params: { query: encodeURIComponent(term) },
+    }),
+  movieDetail: (id) =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  search: (term) =>
+    api.get("search/tv", {
+      params: { query: encodeURIComponent(term) },
+    }),
+  showDetail: (id) =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+};
