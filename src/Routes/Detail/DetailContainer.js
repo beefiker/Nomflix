f27dbe57d22378001ad6c7b7d380e2bc44f1d57d
J young <beefiker@gmail.com>
2021-05-05 00:00:59 +0900

    add fetch selected data(Movie or Show) on DetailContainer

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,11 +1,48 @@
-import React, { useState } from "react";
+import { moviesApi, tvApi } from "api";
+import React, { useEffect, useState } from "react";
 import DetailPresenter from "./DetailPresenter";
 
-const DetailContainer = () => {
+const DetailContainer = (props) => {
+  const {
+    location: { pathname },
+  } = props;
+  const [isMovie, setIsMovie] = useState(pathname.includes("/movie/"));
   // states
   const [result, setResult] = useState([]);
   const [error, setError] = useState([]);
   const [loading, setLoading] = useState("true");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const {
+        match: {
+          params: { id },
+        },
+        history: { push },
+      } = props;
+      // const isMovie = pathname.includes("/movie/");
+      const parsedId = parseInt(id);
+      if (isNaN(parsedId)) {
+        return push("/");
+      }
+      let result = null;
+      try {
+        if (isMovie) {
+          const request = await moviesApi.movieDetail(parsedId);
+          result = request.data;
+        } else {
+          const request = await tvApi.showDetail(parsedId);
+          result = request.data;
+        }
+      } catch {
+        setError("Cant find anything");
+      } finally {
+        setLoading(false);
+        setResult(result);
+      }
+    };
+    fetchData();
+  }, []);
 
   return <DetailPresenter result={result} error={error} loading={loading} />;
 };
